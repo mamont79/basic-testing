43f7af6f57b49b0d00620dfe4b81fd422be2c049
Pavel Dalhou <dalhoupavel@gmail.com>
2024-02-06 16:35:57 +0100

    feat: add simple tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,12 +1,14 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+// import { simpleCalculator, Action } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+// const testCases = [
+//   { a: 1, b: 2, action: Action.Add, expected: 3 },
+//   { a: 2, b: 2, action: Action.Add, expected: 4 },
+//   { a: 3, b: 2, action: Action.Add, expected: 5 },
+//   { a: -5, b: 2, action: Action.Add, expected: -5 },
+//   { a: -3, b: 3, action: Action.Add, expected: 0 },
+//   { a: -3, b: -22, action: Action.Add, expected: -25 },
+// ];
 
 describe('simpleCalculator', () => {
   // This test case is just to run this test suite, remove it when you write your own tests
